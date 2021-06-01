3729ec48aa222aece590d20b7dcc804e2b8adef0
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-06-01 16:06:11 +0530

    Updated day5

--- Day_5_Core_Java_Basics_Programs/Leap.java
+++ Day_5_Core_Java_Basics_Programs/Leap.java
@@ -5,8 +5,8 @@ public class Leap {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the year : ");
         int year = sc.nextInt();
-        int b = year % 4;
-        if (b == 0) {
+
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
             System.out.println("year is leap year");
         } else {
             System.out.println("year is not leap year");
